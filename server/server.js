dad92a0d0eb64343a220a4a397150ca1114fff69
Roman Khokhlov <44736500+adm-dx@users.noreply.github.com>
2019-07-24 16:24:35 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,6 +17,6 @@ app.get('/api/products', (req, res) => {
    })
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening at port ${port} ....`));
 
